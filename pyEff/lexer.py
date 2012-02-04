3d513862d4791c472dd21619f49b73fb7c138f6f
Lovish Kumar <lovish1234@gmail.com>
2012-02-04 20:15:53 +0530

    more functions added to lexer

--- pyEff/lexer.py
+++ pyEff/lexer.py
@@ -39,9 +39,46 @@ Keywords = {
 	'ensure':'ENSURE',
 	'variant':'VARIANT',
 	'invariant':'INVARIANT',
+		#added
+	'agent' : 'AGENT',
+	'alias' : 'ALIAS',
+	'all' : 'ALL',
+	'as' : 'ASSIGN',
+	'attached' : 'ATTACHED',
+	'attribute' : 'ATTRIBUTE',
+	#'check' : 'CHECK',
+	'create' : 'CREATE',
+	'convert' : 'CONVERT',
+	'debug' : 'DEBUG' ,
+	'deferred' : 'DEFERRED',
+	'detachable' :'DETACHABLE',
+	#'ensure' : 'ENSURE',
+	#'expanded' : 'EXPANDED',
+	'export' : 'EXPORT',
+	'external' : 'EXTERNAL',
+	'frozen' : 'FROZEN' ,
+	'implies' : 'IMPLIES',
+	'inherit' : 'INHERIT',
+	'inspect' : 'INSPECT',
+	'loop' : 'LOOP',
+	'note' : 'NOTE',
+	'obsolete' : 'OBSOLETE',
+	'old' : 'OLD',
+	'once' : 'ONCE',
+	'redefine' : 'REDEFINE',
+	'rename' : 'RENAME',
+	'resque' : 'RETRY',
+	'select' : 'SELECT',
+	'seperate' : 'SEPERATE',
+	'undefine' : 'UNDEFINE',
+	
 }
 tokens =  [
-   'NUMBER',
+   'INTEGER',
+   'REAL',
+   'CHARACTER',
+   'BOOLEAN',
+   'ARRAY',
    'PLUS',
    'MINUS',
    'TIMES',
@@ -62,6 +99,10 @@ tokens =  [
    'RPAREN',
    'IDENTIFIER',
    'COMMENT',
+   #added
+   #'BOOL',
+   'DOT',
+   'DQUOTES',
 ]+ list(Keywords.values())
 
 
@@ -84,6 +125,9 @@ t_GREATER = r'>'
 t_GREATER_EQUAL = r'>='
 t_LESS_EQUAL = r'<='
 t_SEPARTOR = r';'
+#added
+t_DOT = r'.'
+t_DQUOTES = r'"'
 
 
 # A regular expression rule with some action code
@@ -91,7 +135,11 @@ t_SEPARTOR = r';'
 def t_COMMENT(t):
     r'[-][-][^\n]+'
     pass
-def t_NUMBER(t):
+def t_REAL(t):
+    r'[\d]*(\.\d*)([e|E](\+|\-)?\d+)?'
+    #r'[\d]+(\.\d+)([e|E](\+|\-)?\d+)?'
+    return t    
+def t_INTEGER(t):
     r'\d+'
     t.value = int(t.value)    
     return t
@@ -99,15 +147,22 @@ def t_OR_ELSE(t):
     r'or\selse'
     t.value = str(t.value)    
     return t
-
 def t_AND_THEN(t):
     r'and\sthen'
     t.value = str(t.value)    
     return t
+#def t_CHARACTER(t):
+ #   r'[a-zA-Z]'
+  #  t.value = str(t.value)    
+   # return t
+def t_BOOL(t):
+	r'[true|false|TRUE|FALSE]'
+	t.value=bool(t.value)
 def t_IDENTIFIER(t):
     r'[a-zA-Z]\w*'
     t.type= Keywords.get(t.value,'IDENTIFIER')    # Check for reserved words   
     return t
+
 
 # Define a rule so we can track line numbers
 def t_newline(t):
@@ -129,7 +184,11 @@ lexer = lex.lex()
 # Test it out
 data = '''
 --3<= 3and d_ sdd3= + 4 and or or or else
-10--
+--10-- 
+--true
+--true TRUE FALSE lovish
+NEGETIVE = -567
+REAL = 3.14159265358979323846 ""
 --  + -20 or 2
 '''
 
